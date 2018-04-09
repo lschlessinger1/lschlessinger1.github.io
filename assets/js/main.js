ce7e3eefa94c5cd1ae5a60335ac8dc6ec9176b18
Louis Schlessinger <louschlessinger@gmail.com>
2018-04-08 20:08:01 -0500

    redesign website

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+$("#main-navbar ul li a[href^='#']").on('click', smoothScroll);
+$("#main-navbar a.navbar-brand[href^='#']").on('click', smoothScroll);
+
+function smoothScroll(e) {
+
+   // prevent default anchor click behavior
+   e.preventDefault();
+
+   // store hash
+   var hash = this.hash;
+
+   // animate
+   $('html, body').animate({
+       scrollTop: $(hash).offset().top
+     }, 1000, function(){
+
+       // when done, add hash to url
+       // (default click behaviour)
+       window.location.hash = hash;
+     });
+
+}
